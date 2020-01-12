d5c223d700c52edaa575e4a0a6c2511948a94fc6
Tony Chou <chou.0626@gmail.com>
2020-01-12 13:38:48 -0500

    Added icon and some validation; need to fix a bug

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,30 @@
 import React, { Component } from "react";
 import Layout from "../components/layout";
 
+const emailRegex = RegExp(
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+);
+
+const formValid = ({ formErrors, ...rest }) => {
+  let valid = true;
+
+  // validate form errors being empty
+  Object.values(formErrors).forEach(val => {
+    // val.length > 0 && (valid = false);
+    if (val.length > 0) {
+      valid = false;
+    }
+  });
+  // validate the form was filled out
+  Object.values(rest).forEach(val => {
+    // val === null && (valid = false);
+    if (val === "") {
+      valid = false;
+    }
+  });
+
+  return valid;
+};
 class Contact extends Component {
   constructor() {
     super();
@@ -9,23 +33,79 @@ class Contact extends Component {
       name: "",
       phone: "",
       email: "",
-      message: ""
+      message: "",
+      formErrors: {
+        company: "",
+        name: "",
+        phone: "",
+        email: "",
+        message: ""
+      }
     };
   }
 
   handleInputChange = event => {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+    const { name, value } = event.target;
+    // const target = event.target;
+    // const value = target.value;
+    // const name = target.name;
+    let formErrors = this.state.formErrors;
+
+    switch (name) {
+      // case "company":
+      //   formErrors.company =
+      //     value.length < 4 && value.length > 0
+      //       ? "minimum 4 characters required"
+      //       : "";
+      //   break;
+
+      case "name":
+        formErrors.name =
+          value.length < 3 ? "minimum 3 characters required" : "";
+        break;
+
+      case "phone":
+        formErrors.phone = value.length < 10 ? "invalid phone number" : "";
+        break;
+
+      case "email":
+        formErrors.email = emailRegex.test(value)
+          ? ""
+          : "invalid email address";
+        break;
+
+      case "message":
+        formErrors.message =
+          value.length < 4 ? "minimum 4 characters required" : "";
+        break;
+      default:
+        break;
+    }
+
     this.setState({
+      formErrors,
       [name]: value
     });
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    console.log(this.inputNode.value);
+    if (formValid(this.state)) {
+      console.log(`
+        --SUBMITTING--
+        Company: ${this.state.company}
+        Name: ${this.state.name}
+        Phone: ${this.state.phone}
+        Email: ${this.state.email}
+        Message: ${this.state.message}
+      `);
+    } else {
+      console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+    }
+
     this.resetForm();
+
     // fetch("http://localhost:3002/send", {
     //   method: "POST",
     //   body: JSON.stringify(this.state),
@@ -57,88 +137,165 @@ class Contact extends Component {
 
   render() {
     const style = {
-      padding: '5rem 1.5rem'
+      padding: "5rem 1.5rem"
     };
+
+    const { formErrors } = this.state;
 
     return (
       <Layout>
-        <section className="section" style={style}>
-        <div className='column is-half is-offset-one-quarter'>
-          <div class="box">
-          <div>
-            <p className='is-size-4'>Get in Touch</p>
-            <p>
-              In Tensure Consulting, our experienced consultants and designers
-              strive to achieve technology innovations and to inspire next
-              generation developers.
-            </p>
-          </div>
-          <hr />
-          <form
-            id='contact-form'
-            onSubmit={this.handleSubmit}
-            method='post'
-            action='#'
-          >
-            <div className='field'>
-              <label htmlFor='compnay'>Company</label>
-              <input
-                type='text'
-                className='input is-success'
-                name='company'
-                value={this.state.company}
-                onChange={this.handleInputChange}
-              />
-            </div>
-            <div className='field'>
-              <label htmlFor='name'>Name</label>
-              <input
-                type='text'
-                className='input is-success'
-                name='name'
-                value={this.state.name}
-                onChange={this.handleInputChange}
-              />
-            </div>
-            <div className='field'>
-              <label htmlFor='phone'>Phone</label>
-              <input
-                type='text'
-                className='input is-success'
-                name='phone'
-                value={this.state.phone}
-                onChange={this.handleInputChange}
-              />
-            </div>
-            <div className='field'>
-              <label htmlFor='exampleInputEmail1'>Email address</label>
-              <input
-                type='email'
-                className='input is-success'
-                aria-describedby='emailHelp'
-                name='email'
-                value={this.state.email}
-                onChange={this.handleInputChange}
-              />
-            </div>
-            <div className='field'>
-              <label htmlFor='message'>Message</label>
-              <div className='control'>
-                <textarea
-                  className='textarea'
-                  placeholder='Please describe'
-                  name='message'
-                  value={this.state.message}
-                  onChange={this.handleInputChange}
-                ></textarea>
+        <section className='section' style={style}>
+          <div className='column is-half is-offset-one-quarter'>
+            <div className='box'>
+              <div>
+                <p className='is-size-4'>Get in Touch</p>
+                <p>
+                  In Tensure Consulting, our experienced consultants and
+                  designers strive to achieve technology innovations and to
+                  inspire next generation developers.
+                </p>
               </div>
+              <hr />
+              <form
+                id='contact-form'
+                onSubmit={this.handleSubmit}
+                noValidate
+                method='post'
+                action='#'
+              >
+                <div className='field'>
+                  <label htmlFor='compnay'>
+                    Company:
+                    <div className='control has-icons-left'>
+                      <input
+                        type='text'
+                        className='input is-success'
+                        placeholder='Compnay Name'
+                        name='company'
+                        value={this.state.company}
+                        onChange={this.handleInputChange}
+                        ref={node => (this.inputNode = node)}
+                        noValidate
+                      />
+                      <span className='icon is-small is-left'>
+                        <i className='fas fa-building'></i>
+                      </span>
+                    </div>
+                  </label>
+                </div>
+                <div className='field'>
+                  <label htmlFor='name'>
+                    Name:
+                    <div className='control has-icons-left'>
+                      <input
+                        type='text'
+                        className={
+                          formErrors.name.length > 0
+                            ? "input is-danger"
+                            : "input is-success"
+                        }
+                        placeholder='Name'
+                        name='name'
+                        value={this.state.name}
+                        onChange={this.handleInputChange}
+                        ref={node => (this.inputNode = node)}
+                        noValidate
+                      />
+                      <span className='icon is-small is-left'>
+                        <i className='fas fa-user'></i>
+                      </span>
+                    </div>
+                    {formErrors.name.length > 0 && (
+                      <span className='help is-danger'>{formErrors.name}</span>
+                    )}
+                  </label>
+                </div>
+                <div className='field'>
+                  <label htmlFor='phone'>
+                    Phone:
+                    <div className='control has-icons-left'>
+                      <input
+                        type='text'
+                        className={
+                          formErrors.phone.length > 0
+                            ? "input is-danger"
+                            : "input is-success"
+                        }
+                        placeholder='(xxx) xxx-xxxx'
+                        name='phone'
+                        value={this.state.phone}
+                        onChange={this.handleInputChange}
+                        ref={node => (this.inputNode = node)}
+                        noValidate
+                      />
+                      <span className='icon is-small is-left'>
+                        <i className='fas fa-phone'></i>
+                      </span>
+                    </div>
+                    {formErrors.phone.length > 0 && (
+                      <span className='help is-danger'>{formErrors.name}</span>
+                    )}
+                  </label>
+                </div>
+                <div className='field'>
+                  <label htmlFor='exampleInputEmail1'>
+                    Email:
+                    <div className='control has-icons-left has-icons-right'>
+                      <input
+                        type='email'
+                        className={
+                          formErrors.email.length > 0
+                            ? "input is-danger"
+                            : "input is-success"
+                        }
+                        placeholder='Email'
+                        aria-label='Email'
+                        aria-describedby='emailHelp'
+                        name='email'
+                        value={this.state.email}
+                        onChange={this.handleInputChange}
+                        ref={node => (this.inputNode = node)}
+                        noValidate
+                      />
+                      <span className='icon is-small is-left'>
+                        <i className='fas fa-envelope'></i>
+                      </span>
+                      <span className='icon is-small is-right'>
+                        <i className='fas fa-exclamation-triangle'></i>
+                      </span>
+                    </div>
+                    {formErrors.email.length > 0 && (
+                      <span className='help is-danger'>{formErrors.email}</span>
+                    )}
+                  </label>
+                </div>
+                <div className='field'>
+                  <label htmlFor='message'>
+                    Message:
+                    <div className='control'>
+                      <textarea
+                        className='textarea '
+                        placeholder='Please describe'
+                        name='message'
+                        value={this.state.message}
+                        onChange={this.handleInputChange}
+                        ref={node => (this.inputNode = node)}
+                        noValidate
+                      ></textarea>
+                    </div>
+                    {formErrors.message.length > 0 && (
+                      <span className='help is-danger'>
+                        {formErrors.message}
+                      </span>
+                    )}
+                  </label>
+                </div>
+                <button type='submit' className='button is-primary'>
+                  Submit
+                </button>
+              </form>
             </div>
-            <button type='submit' className='button is-primary'>
-              Submit
-            </button>
-          </form>
           </div>
-        </div>
         </section>
       </Layout>
     );
