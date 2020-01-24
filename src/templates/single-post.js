07fd6fb4e593eb9312942cc74c68b7d97c8f2d88
Tony Chou <chou.0626@gmail.com>
2020-01-24 10:43:04 -0500

    Added share posts

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -6,72 +6,132 @@ import SEO from "../components/seo";
 import { Badge, Card, CardBody, CardSubtitle, Row, Col } from "reactstrap";
 import Img from "gatsby-image";
 import { slugify } from "../util/utilityFunctions";
-import authors from '../util/authors'
+import authors from "../util/authors";
+import './single-post.scss'
 
-const SinglePost = ({ data }) => {
+const SinglePost = ({ data, pageContext }) => {
   const post = data.markdownRemark.frontmatter;
-  const author = authors.find(x => x.name === post.author)
+  const author = authors.find(x => x.name === post.author);
+
+  const baseUrl = "http://gatsbytutorial.co.uk/";
+
   return (
     <Layout pageTitle={post.title}>
-      <div className="container">
-      <SEO title={post.title} />
-      <Row>
-        <Col md='8'>
-          <Card>
-            <Img
-              className='card-image-top'
-              fluid={post.image.childImageSharp.fluid}
-            />
-            <CardBody>
-              <CardSubtitle>
-                <span className='text-info'>{post.date}</span> by{" "}
-                <span className='text-info'>{post.author}</span>
-              </CardSubtitle>
-              <div
-                dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+      <div className='container'>
+        <SEO title={post.title} />
+        <Row>
+          <Col md='8'>
+            <Card>
+              <Img
+                className='card-image-top'
+                fluid={post.image.childImageSharp.fluid}
               />
-              <ul className='post-tags'>
-                {post.tags.map(tag => (
-                  <li key={tag}>
-                    <Link to={`/tag/${slugify(tag)}`}>
-                      <Badge color='primary'>{tag}</Badge>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </CardBody>
-          </Card>
-        </Col>
-        <Col md='4'>
-          <Sidebar postAuthor={author} authorImageFluid={data.file.childImageSharp.fluid}/>
-        </Col>
-      </Row>
-    </div>
+              <CardBody>
+                <CardSubtitle>
+                  <span className='text-info'>{post.date}</span> by{" "}
+                  <span className='text-info'>{post.author}</span>
+                </CardSubtitle>
+                <div
+                  dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+                />
+                <ul className='post-tags'>
+                  {post.tags.map(tag => (
+                    <li key={tag}>
+                      <Link to={`/tag/${slugify(tag)}`}>
+                        <Badge color='primary'>{tag}</Badge>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </CardBody>
+            </Card>
+            <h1>Share this post</h1>
+            <ul>
+              <li>
+                <a
+                  href={
+                    "https://www.facebook.com/sharer/sharer.php?u=" +
+                    baseUrl +
+                    pageContext.slug
+                  }
+                  className='facebook'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <i className='fab fa-facebook-f fa-2x'></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href={
+                    "https://www.twitter.com/share?url=" + baseUrl + pageContext.slug + '&text=' + post.title + '&via' + 'twitterHandle'
+                  }
+                  className='twitter'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <i className='fab fa-twitter fa-2x'></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href={
+                    "https://plus.google.com/share?url=" + baseUrl + pageContext.slug
+                  }
+                  className='google'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <i className='fab fa-google fa-2x'></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href={
+                    "https://www.linkedin.com/shareArticle?url=" + baseUrl + pageContext.slug
+                  }
+                  className='linkedin'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <i className='fab fa-linkedin fa-2x'></i>
+                </a>
+              </li>
+            </ul>
+          </Col>
+          <Col md='4'>
+            <Sidebar
+              postAuthor={author}
+              authorImageFluid={data.file.childImageSharp.fluid}
+            />
+          </Col>
+        </Row>
+      </div>
     </Layout>
   );
 };
 
 export const postQuery = graphql`
   query blogPostBySlug($slug: String!, $imageUrl: String!) {
-    markdownRemark(fields: { slug: { eq: $slug }}) {
-          id
-          html
-          frontmatter {
-            title
-            author
-            date(formatString: "MMM Do YYYY")
-            tags
-            image {
-              childImageSharp {
-                fluid(maxWidth: 500) {
-                  ...GatsbyImageSharpFluid
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      html
+      frontmatter {
+        title
+        author
+        date(formatString: "MMM Do YYYY")
+        tags
+        image {
+          childImageSharp {
+            fluid(maxWidth: 500) {
+              ...GatsbyImageSharpFluid
             }
           }
         }
       }
     }
-    file(relativePath: { eq: $imageUrl}){
-      childImageSharp{
+    file(relativePath: { eq: $imageUrl }) {
+      childImageSharp {
         fluid(maxWidth: 300) {
           ...GatsbyImageSharpFluid
         }
