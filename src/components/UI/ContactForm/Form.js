abd89d33bd289c000ca637371a62c7a33acf07f8
Tony Chou <chou.0626@gmail.com>
2020-01-30 08:24:57 -0500

    Form Submission message.

--- src/components/UI/ContactForm/Form.js
+++ src/components/UI/ContactForm/Form.js
@@ -5,7 +5,13 @@ import { FirebaseContext } from "gatsby-plugin-firebase";
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-import { FormControl, TextField, InputLabel } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  FormControl,
+  TextField,
+  Typography
+} from "@material-ui/core";
 
 const styles = theme => ({
   container: {
@@ -20,6 +26,20 @@ const styles = theme => ({
     display: "flex",
     flexWrap: "wrap"
   },
+  card: {
+    minWidth: 275
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 20
+  },
+  pos: {
+    marginBottom: 12
+  }
 });
 
 const ContactForm = (props, { className }) => {
@@ -35,7 +55,6 @@ const ContactForm = (props, { className }) => {
   const [isSubmitted, setIsSubmitted] = useState(false);
 
   function submit() {
-    debugger;
     firebase
       .firestore()
       .collection("contacts")
@@ -45,128 +64,150 @@ const ContactForm = (props, { className }) => {
 
   return (
     <div>
-      {/* {!isSubmitted ? ( */}
-      <section>
-        <Container className={classes.container}>
-          <form
-            id='contact-form'
-            onSubmit={event => handleSubmit(event, submit)}
-            noValidate
-            method='post'
-            action='#'
-          >
-            <FormControl fullWidth className={classes.margin} variant='filled'>
-              <TextField
-                fullwidth
-                variant="outlined"
-                type='text'
-                name='company'
-                style={{marginBottom: 10 }}
-                value={values.company}
-                label='Company name'
-                onChange={handleInputChange}
-                noValidate
-              />
-            </FormControl>
-            <FormControl fullWidth className={classes.margin} variant='filled'>
-              <TextField
-                required
-                id='standard-full-width'
-                variant="outlined"
-                type='text'
-                error={errors.name != ""}
-                name='name'
-                style={{marginBottom: 10 }}
-                label='Full name'
-                value={values.name}
-                onChange={handleInputChange}
-                helperText={errors.name}
-              />
-            </FormControl>
-            <FormControl fullWidth className={classes.margin} variant='filled'>
-              <TextField
-                required
-                id='standard-full-width'
-                variant="outlined"
-                type='text'
-                error={errors.phone != ""}
-                placeholder='###-###-####'
-                name='phone'
-                style={{marginBottom: 10 }}
-                label='Phone number'
-                value={values.phone}
-                onChange={handleInputChange}
-                helperText={errors.phone}
-              />
-            </FormControl>
-            <FormControl fullWidth className={classes.margin} variant='filled'>
-              <TextField
-                required
-                id='standard-full-width'
-                variant="outlined"
-                type='email'
-                error={errors.email != ""}
-                aria-label='Email'
-                aria-describedby='emailHelp'
-                name='email'
-                style={{marginBottom: 10 }}
-                value={values.email}
-                label='Email address'
-                onChange={handleInputChange}
-                helperText={errors.email}
-              />
-            </FormControl>
-            <FormControl fullWidth className={classes.margin} variant='filled'>
-              <TextField
-                required
-                id='standard-full-width'
-                variant="outlined"
-                multiline
-                rows='10'
-                error={errors.message != ""}
-                name='message'
-                style={{marginBottom: 10 }}
-                value={values.message}
-                label='How can we help you?'
-                onChange={handleInputChange}
-                helperText={errors.message}
-                variant='outlined'
-              ></TextField>
-            </FormControl>
-            <div className='buttons'>
-            <Button fullWidth type='submit' variant="contained" color="primary" size="large">
-              Submit
-            </Button>
-              {/* <Button type='submit' variant='contained' color='primary'>
-                Confirm & Send
-              </Button> */}
-            </div>
-          </form>
-        </Container>
-      </section>
-      {/* // ) : (
-    //     <section className='section'>
-    //       <div className='column is-half is-offset-one-quarter'>
-    //         <article className='message is-primary'>
-    //           <div className='message-header'>
-    //             <p>Thank you for contacting us!</p>
-    //           </div>
-    //           <div className='message-body'>
-    //             Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
-    //             <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
-    //             porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla.
-    //             Nullam gravida purus diam, et dictum <a>felis venenatis</a>{" "}
-    //             efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus.
-    //             Donec sodales, arcu et sollicitudin porttitor, tortor urna
-    //             tempor ligula, id porttitor mi magna a neque. Donec dui urna,
-    //             vehicula et sem eget, facilisis sodales sem.
-    //           </div>
-    //         </article>
-    //       </div>
-    //     </section>
-    //   )}
-    // </div>
-  // ); */}
+      {!isSubmitted ? (
+        <section>
+          <Container className={classes.container}>
+            <form
+              id='contact-form'
+              onSubmit={event => handleSubmit(event, submit)}
+              noValidate
+              method='post'
+              action='#'
+            >
+              <FormControl
+                fullWidth
+                className={classes.margin}
+                variant='filled'
+              >
+                <TextField
+                  variant='outlined'
+                  type='text'
+                  name='company'
+                  style={{ marginBottom: 10 }}
+                  value={values.company}
+                  label='Company name'
+                  onChange={handleInputChange}
+                  noValidate
+                />
+              </FormControl>
+              <FormControl
+                fullWidth
+                className={classes.margin}
+                variant='filled'
+              >
+                <TextField
+                  required
+                  id='standard-full-width'
+                  variant='outlined'
+                  type='text'
+                  error={errors.name !== ""}
+                  name='name'
+                  style={{ marginBottom: 10 }}
+                  label='Full name'
+                  value={values.name}
+                  onChange={handleInputChange}
+                  helperText={errors.name}
+                />
+              </FormControl>
+              <FormControl
+                fullWidth
+                className={classes.margin}
+                variant='filled'
+              >
+                <TextField
+                  required
+                  id='standard-full-width'
+                  variant='outlined'
+                  type='text'
+                  error={errors.phone !== ""}
+                  placeholder='###-###-####'
+                  name='phone'
+                  style={{ marginBottom: 10 }}
+                  label='Phone number'
+                  value={values.phone}
+                  onChange={handleInputChange}
+                  helperText={errors.phone}
+                />
+              </FormControl>
+              <FormControl
+                fullWidth
+                className={classes.margin}
+                variant='filled'
+              >
+                <TextField
+                  required
+                  id='standard-full-width'
+                  variant='outlined'
+                  type='email'
+                  error={errors.email !== ""}
+                  aria-label='Email'
+                  aria-describedby='emailHelp'
+                  name='email'
+                  style={{ marginBottom: 10 }}
+                  value={values.email}
+                  label='Email address'
+                  onChange={handleInputChange}
+                  helperText={errors.email}
+                />
+              </FormControl>
+              <FormControl
+                fullWidth
+                className={classes.margin}
+                variant='filled'
+              >
+                <TextField
+                  required
+                  id='standard-full-width'
+                  variant='outlined'
+                  multiline
+                  rows='10'
+                  error={errors.message !== ""}
+                  name='message'
+                  style={{ marginBottom: 10 }}
+                  value={values.message}
+                  label='How can we help you?'
+                  onChange={handleInputChange}
+                  helperText={errors.message}
+                  variant='outlined'
+                ></TextField>
+              </FormControl>
+              <div className='buttons'>
+                <Button
+                  fullWidth
+                  type='submit'
+                  variant='contained'
+                  color='primary'
+                  size='large'
+                >
+                  Submit
+                </Button>
+                <Button type='submit' variant='contained' color='primary'>
+                  Confirm & Send
+                </Button>
+              </div>
+            </form>
+          </Container>
+        </section>
+      ) : (
+        <section className='section'>
+          <Container className='container'>
+            <Card className={classes.card} variant='outlined'>
+              <CardContent>
+                <Typography
+                  className={classes.title}
+                  color='textSecondary'
+                  gutterBottom
+                >
+                  Thank you for contacting us!
+                </Typography>
+                <Typography color='textSecondary'>
+                  We will get back to you as soon as possible!
+                </Typography>
+              </CardContent>
+            </Card>
+          </Container>
+        </section>
+      )}
     </div>
   );
 };
