9797e1625169ceb01cbbe92e22eeacf529549ef9
Joe Kerstanoff <joe.kerstanoff@gmail.com>
2020-01-30 20:21:56 -0500

    y tho

--- src/components/layout-homepage.js
+++ src/components/layout-homepage.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Footer from "./footer.js";
+import Footer from "./footer";
 import Hero from "./hero";
-import NavBar from "./navbar.js";
+import NavBar from "./navbar";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
-import Footer from "./footer.js";
+import Footer from "./footer";
 import { useStaticQuery, graphql } from "gatsby";
 import layoutStyles from "./layout.module.scss";
 
