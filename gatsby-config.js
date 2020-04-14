8b83c696d8dbdb707c474db8f104398de9a627b5
Chris Costanza <chris@mindboxstudios.com>
2020-04-14 16:44:29 -0400

    Google Landing Page Development

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,6 +79,14 @@ module.exports = {
         icon: `src/images/tensure2020_favicon_16x16.png` // This path is relative to the root of the site.
       }
     },
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        typekit: {
+          id: 'suw2nxe'
+        }
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`
